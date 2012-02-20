511260f987b8839dc08d7b03ec6476614e3a5e47
Joe <cpmhjoe@gmail.com>
2012-02-20 15:34:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goldenseal::Application.config.secret_token = '231756d04e10017be5d938003655585b642720232895d13498832d5b7098214c9fccc8b51fcbd3337cae552fe62fd6e02aa228bc22ff8587bd594107c2e16a71'

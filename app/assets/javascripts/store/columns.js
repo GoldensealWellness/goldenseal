26e885c33fb0e9bec4893d134fecca4fc9be0a4e
Joe <cpmhjoe@gmail.com>
2012-03-12 16:45:30 -0400

    finished styles

--- app/assets/javascripts/store/columns.js
+++ app/assets/javascripts/store/columns.js
@@ -1,6 +1,24 @@
-$(document).ready(function() {
-			var height = $("#sidebar").height();
-			if (height > 400) {
-			  $("#content").css("min-height", height + 10 + 'px');
-			}
+$(document).ready(function(){
+  $(function() {
+	  var height = $("#sidebar").height();
+		if (height > 400) {
+			$("#content").css("min-height", height + 10 + 'px');
+		}
+  });
+  
+  $(function() {
+    if ($('#sidebar').length == 0) {
+      $("#content").css("margin-left", "auto");
+    }
+  });
+  
+  $(function() {
+    if ($('#sidebar').length == 0) {
+      var width = $("#wrapper").width();
+      $("#content").css("margin-left", "auto");
+    } else {
+      var width = $("#content").width() + 40;
+    };
+    $("footer").css("max-width", width);
+  });
 });

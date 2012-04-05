65581766f391a6c54fc883f44e75fb887b9c2bda
Joe <cpmhjoe@gmail.com>
2012-04-05 15:58:49 -0400

    restricted products complete

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,4 +2,8 @@ Spree::Product.class_eval do
   has_many :blurbs, :class_name => "Spree::Blurb"
   has_many :product_allowed_roles, :class_name => "Spree::ProductAllowedRole"
   has_many :roles, :class_name => "Spree::Role", :through => :product_allowed_roles
+  
+  def all_roles?
+    roles.count == Spree::Role.count
+  end
 end

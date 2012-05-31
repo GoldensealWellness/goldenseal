fb4272836ac05bb79b4108b443aed7a8ee6ae845
Joe <cpmhjoe@gmail.com>
2012-05-31 03:43:54 -0400

    override sequence

--- app/overrides/sort_bar.rb
+++ app/overrides/sort_bar.rb
@@ -2,4 +2,5 @@ Deface::Override.new(:virtual_path => "spree/products/index",
                       :name => "show_search_partials_sort_bar",
                       :insert_top => "[data-hook='search_results']",
                       :partial => "spree/products/sort_bar",
+                      :sequence => 150,
                       :disabled => false)

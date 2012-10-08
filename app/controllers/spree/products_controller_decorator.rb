30d9b31469938381ca2fed8b90c238a18675e124
Kyle Ries <kyle.ries@gmail.com>
2012-10-08 11:41:12 -0600

    Add SSL in staging. Test product show page change

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,5 +1,5 @@
 Spree::ProductsController.class_eval do
-     def index
+    def index
       @searcher = Spree::Config.searcher_class.new(params)
       @products = @searcher.retrieve_products
       respond_with(@products)
